8395774b3d039fb9f567f5f43d44dc645b5dd156
Jephthah Sadare <jephsadare@gmail.com>
2022-06-30 12:39:15 +0100

    AF-1 created first actor

--- src/main/java/com/example/AkkaStart.java
+++ src/main/java/com/example/AkkaStart.java
@@ -1,4 +1,10 @@
 package com.example;
 
+import akka.actor.typed.ActorSystem;
+
 public class AkkaStart {
+
+    public static void main (String[] args) {
+        ActorSystem.create(IotSupervisor.create(), "iot-system");
+    }
 }
