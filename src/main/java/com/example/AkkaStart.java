8dbad49fd4dc6e311f9bac516df69d37ec1d1ee1
Jephthah Sadare <jephsadare@gmail.com>
2022-07-01 08:37:31 +0100

    merge fix

--- src/main/java/com/example/AkkaStart.java
+++ src/main/java/com/example/AkkaStart.java
@@ -1,12 +1,9 @@
 package com.example;
 
 public class AkkaStart {
-<<<<<<< HEAD
-=======
 
     public static void main (String[] args) {
 //        start actor system
         ActorSystem.create(IotSupervisor.create(), "iot-system");
     }
->>>>>>> AF-2-working-with-device-actors
 }
